0c4f6746d509f21c91764ac4638ff62c9daead17
Sidrishe <xomikxam@mail.ru>
2023-09-11 17:50:07 +0300

    Добавил функцию

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 
+from catalog.models import Product, Category
+
+
+# def home(request):
+#     context = {
+#         'object_list': Category.objects.all(),
+#         'title': 'Каталог товаров'
+#     }
+#     return render(request, 'catalog/home.html', context)
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list.all(),
+        'title': 'Главная'
+    }
+    return render(request, 'catalog/home.html', context)
+
+
+def products(request, pk):
+    category_name = Product.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(category=pk),
+        'title': f'Каталог товаров - {category_name.name}'
+    }
+    return render(request, 'catalog/products.html', context)
 
 
 def contacts(request):
@@ -13,3 +36,13 @@ def contacts(request):
         print(f'Имя: {name}\nТелефон: {phone}\nСообщение: {message}')
 
     return render(request, 'catalog/contacts.html')
+
+
+def product(request, pk):
+    products_item = Product.objects.get(pk=pk)
+    products_list = Product.objects.filter(pk=pk)
+    context = {
+        'object_list': products_list,
+        'title': f'Продукт - {products_item}'
+    }
+    return render(request, 'catalog/cur_prod.html', context)
