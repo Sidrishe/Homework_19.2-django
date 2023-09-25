aaabc297fceea294f76dc91879537f0bfd8d895b
Sidrishe <xomikxam@mail.ru>
2023-09-25 18:32:38 +0300

    Прописал модель

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Заголовок')
+    slug = models.CharField(max_length=150, verbose_name='slug', **NULLABLE)
+    text = models.TextField(verbose_name='Содержимое')
+    blog_img = models.ImageField(upload_to='blog/', verbose_name='Превью', **NULLABLE)
+    created_at = models.DateField(auto_now_add=True, **NULLABLE, verbose_name='Дата создания')
+    is_published = models.BooleanField(default=True, verbose_name='Признак публикации')
+    views_count = models.IntegerField(default=0, verbose_name='количество просмотров')
+
+    def __str__(self):
+        return (f'Название: {self.title}'
+                f'Дата создания: {self.created_at}'
+                f'Опубликовано: {self.is_published}')
+
+    class Meta:
+        verbose_name = 'статья'
+        verbose_name_plural = 'статьи'
