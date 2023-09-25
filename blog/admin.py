9fc5c1cce07454a569eb3053c12d4c0f57adc4a0
Sidrishe <xomikxam@mail.ru>
2023-09-25 18:31:44 +0300

    Прописал админку

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from blog.models import Blog
 
-# Register your models here.
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'text',)
+    list_filter = ('title', 'created_at',)
