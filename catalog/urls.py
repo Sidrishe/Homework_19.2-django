cae71b46702f137e38d62cc0023deed37c3c8eb1
Sidrishe <xomikxam@mail.ru>
2023-09-25 18:34:16 +0300

    Прописал url

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from catalog.views import home, contacts,  product
+from catalog.views import contacts, HomeView, ProductListView, ProductDetailView
+
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts),
-    path('<int:pk>/product/', product, name='product'),
+    path('', HomeView.as_view(), name='home'),
+    path('contacts/', contacts, name='contact'),
+    path('<int:pk>/product/', ProductListView.as_view(), name='products'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
 ]
