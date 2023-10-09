b1129282b55b35c669e4039d8e3d2b13e4bdf52a
Sidrishe <xomikxam@mail.ru>
2023-10-09 19:38:02 +0300

    Update

--- catalog/models.py
+++ catalog/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     description = models.TextField(max_length=250, **NULLABLE, verbose_name='Описание')
     image = models.ImageField(upload_to='catalog/', **NULLABLE, verbose_name='Изображение')
     category = models.ForeignKey(Category, **NULLABLE, on_delete=models.CASCADE, verbose_name='Категория')
-    price = models.IntegerField(**NULLABLE, verbose_name='Цена за покупку')
+    price = models.IntegerField(**NULLABLE, verbose_name='Цена')
     created_at = models.DateField(**NULLABLE, verbose_name='Дата создания')
     updated_at = models.DateField(**NULLABLE, verbose_name='Дата последнего изменения')
 
@@ -30,3 +30,17 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
+
+class Version(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='Продукт')
+    version_number = models.CharField(max_length=50, verbose_name='Номер версии', **NULLABLE)
+    version_name = models.CharField(max_length=50, verbose_name='Название версии')
+    is_active = models.BooleanField(verbose_name='Признак текущей версии', default=False)
+
+    def __str__(self):
+        return f'{self.version_number} - {self.version_name}'
+
+    class Meta:
+        verbose_name = 'версия'
+        verbose_name_plural = 'версии'
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 
-from catalog.views import contacts, HomeView, ProductListView, ProductDetailView
+from catalog.apps import CatalogConfig
+from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('', ProductListView.as_view(), name='home'),
     path('contacts/', contacts, name='contact'),
-    path('<int:pk>/product/', ProductListView.as_view(), name='products'),
+    # path('product/', ProductListView.as_view(), name='products'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('product/create/', ProductCreateView.as_view(), name='product_create'),
+    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product_update'),
+    path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='product_delete'),
 ]
